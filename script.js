38ce7b176429d03f28fa0e4bfb34be3adc3b571a
aryanchoudhary1313 <118419410+aryanchoudhary1313@users.noreply.github.com>
2022-12-01 02:42:20 +0530

    Clock

---
+++ script.js
@@ -0,0 +1,16 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+let day = new Date();
+let hh = day.getHours() * 30;
+let mm = day.getMinutes() * deg;
+let ss = day.getSeconds() * deg;
+
+setInterval (() => {
+
+hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+mn.style.transform = `rotateZ(${mm}deg)`;
+sc.style.transform = `rotateZ(${ss}deg)`;
+})
